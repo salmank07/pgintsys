609d80fe2b73efaea4ff5bb84acc73ed2f921093
salman <mohamedsalmankhan509>
2022-08-28 12:20:01 +0530

    aug 28, final changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import * as AOS from 'aos';
+import jump from 'jump.js';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'pgintsys';
+
+  ariaExpanded: boolean = false;
+  windowScrolled: boolean = false;
+
+  constructor() {
+    AOS.init({ duration: 1000 });
+  }
+
+  ngOnInit() { }
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll = ($event: any) => {
+    const verticalOffset = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+
+    if (verticalOffset > 50) {
+      document.getElementById("mainNav").classList.add('navbar-shrink');
+      this.windowScrolled = true;
+    } else if (this.windowScrolled && verticalOffset < 10) {
+      this.windowScrolled = false;
+      document.getElementById("mainNav").classList.remove('navbar-shrink');
+    }
+  }
+
+  clickMenu = () => {
+    this.ariaExpanded = !this.ariaExpanded;
+  }
+
+  scrollToTarget = (target: string) => {
+    const menuButtonElement = document.getElementById("menu-button");
+    menuButtonElement.setAttribute("aria-expanded", "false");
+    document.getElementById("navbarSupportedContent").classList.remove('show');
+    this.ariaExpanded = false;
+    jump(target, {
+      offset: -50
+    });
+  }
+
+  scrollToTop = () => {
+    jump('.landingPage');
+  }
+
 }
